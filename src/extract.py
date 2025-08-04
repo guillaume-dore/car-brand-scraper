eda7a9f773ba105f23f078639d856e614ce3d417
Guillaume Doré <guillaumedore.76@hotmail.fr>
2025-08-04 16:46:29 +0200

    save scraped data in json file. Stop scraping on brand with no vehicle registered found on website.

--- src/extract.py
+++ src/extract.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 import os
 from typing import cast
 from bs4.element import Tag
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import requests
 import re
 import time
+import json
 
 @dataclass
 class CarBrand:
@@ -26,25 +27,23 @@ def extract_logo_url(html_link: Tag) -> str | None:
     
 load_dotenv()
 url = os.getenv("SITE_URL")
+if url is None:
+    print("SITE_URL is not set in the environment variables.")
+    exit(1)
 
 params = {
-    "page": 0,
+    "page": 12,
     "search_text": "",
     "order": "owner_count",
     "view": "list_cards"
 }
 
-headers = {
-    "Content-Type": "application/json"
-}
+headers = { "Content-Type": "application/json" }
 
 car_brands = []
 
 while True:
-    print(f"Fetching page {params['page']}")
-    if url is None:
-        print("SITE_URL is not set in the environment variables.")
-        break
+    print(f"Fetching page {params['page'] + 1}")
     response = requests.get(url, params = params, headers = headers)
 
     if response.status_code != 200:
@@ -60,7 +59,15 @@ while True:
     soup = BeautifulSoup(data.get("brand_recs_html"), "html.parser")
     car_brands_html = soup.select('div.brand_item')
 
+    dead_brand = False
     for brand_html in car_brands_html:
+        registered_cars = brand_html.select_one('div.tooltip_box.tooltip_topright.small.no_click')
+        if registered_cars is not None:
+            dead_brand = cast(Tag, registered_cars).contents[0].get_text(strip=True) == "0"
+
+        if dead_brand:
+            break
+
         contents = brand_html.find_all("a")
 
         if len(contents) == 2:
@@ -70,10 +77,19 @@ while True:
             logo = None
             name = contents[0].get_text(strip=True)
 
-        print(logo, name)
         car_brands.append(CarBrand(logo=logo, name=name))
     
+    if dead_brand:
+        print("Dead brand found, stopping the scraping.")
+        break
+
     params["page"] += 1
     time.sleep(2)
 
-print("Exit program.")
+car_brands_dict = [asdict(brand) for brand in car_brands]
+
+with open("data/car_brands.json", "w", encoding="utf-8") as file:
+    json.dump(car_brands_dict, file, ensure_ascii=False, indent=2)
+
+print(f"Saved {len(car_brands)} car brands to ./data/car_brands.json")
+print("End of program execution.")
